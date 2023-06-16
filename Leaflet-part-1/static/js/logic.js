307b87dfa6df0b0cdfff769da4810d66955f3633
ferrjess <125903996+ferrjess@users.noreply.github.com>
2023-06-15 20:29:54 -0500

    added pushy

---
+++ Leaflet-part-1/static/js/logic.js
@@ -0,0 +1,125 @@
+// Store our API endpoint as queryUrl.
+let queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+// Perform a GET request to the query URL/
+d3.json(queryUrl).then(function (data) {
+  // Once we get a response, send the data.features object to the createFeatures function.
+  createFeatures(data.features);
+  console.log(data.features);
+});
+
+// A function to determine the marker size based on the magntitude
+function markerSize(magnitude) {
+    return (magnitude) * 5;
+  }
+  
+//Create a function for Eathquake color based on depth
+function markerColor(depth) {
+    if (depth <= 100) {
+        return "#e5c0be"
+    } else if (depth <= 200) {
+        return "#ebbdba"
+    } else if (depth <= 300) {
+        return "#ee9e98"
+    } else if (depth <= 400) {
+        return "#ef8d86"
+    } else if (depth <= 500) {
+        return "#e76f66"
+    } else {
+        return "#f24235"
+    }
+};
+
+function createFeatures(earthquakeData) {
+
+  // Define a function we want to run once for each feature in the features array
+  // Give each feature a popup describing the place and time of the earthquake
+  function onEachFeature(feature, layer) {
+    layer.bindPopup("<h3>" + feature.properties.place +
+      "</h3><hr><p>" + new Date(feature.properties.time) + "</p>" + "</h3><hr><p>" + "Magnitude: " + (feature.properties.mag) + "</p>"+"</h3><hr><p>" +"Depth: " + (feature.geometry.coordinates[2]) +"<p>");
+  }
+
+  // Create a GeoJSON layer containing the features array on the earthquakeData object
+  // Run the onEachFeature function once for each piece of data in the array
+  var earthquakes = L.geoJSON(earthquakeData, {
+    pointToLayer: function(feature, latlng) {
+        return L.circleMarker(latlng, {
+            radius: markerSize(feature.properties.mag),
+            fillColor: markerColor(feature.geometry.coordinates[2]),
+            color: "#000",
+            weight: 0.3,
+            opacity: 0.5,
+            fillOpacity: 1
+        });
+    },
+    onEachFeature: onEachFeature
+  });
+  // Send our earthquakes layer to the createMap function/
+  createMap(earthquakes);
+}
+
+function createMap(earthquakes) {
+
+  // Create the base layers.
+  let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  })
+
+  let topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+    attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+  });
+
+  // Create a baseMaps object.
+  let baseMaps = {
+    "Street Map": street,
+    "Topographic Map": topo
+  };
+
+  // Create an overlay object to hold our overlay.
+  let overlayMaps = {
+    Earthquakes: earthquakes
+  };
+
+  // Create our map, giving it the streetmap and earthquakes layers to display on load.
+  let myMap = L.map("map", {
+    center: [
+      37.09, -95.71
+    ],
+    zoom: 5,
+    layers: [street, earthquakes]
+  });
+
+  // Create a layer control.
+  // Pass it our baseMaps and overlayMaps.
+  // Add the layer control to the map.
+  L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
+  }).addTo(myMap);
+
+
+  // Set up the legend.
+  let legend = L.control({ position: "bottomright" });
+  legend.onAdd = function() {
+    let div = L.DomUtil.create("div", "info legend");
+    let depths = [0,100, 200, 300, 500]
+    let labels = [];
+
+
+
+    legendInfo = "<h1>Earthquake Depths</h1>";
+
+    div.innerHTML = legendInfo;
+
+    for (let i = 0; i < depths.length; i++) {
+      labels.push('<li style="background-color:' + markerColor(depths[i] + 1) + ' ;list-style-type:none"> <span>' + depths[i] + (depths[i + 1]
+           ? '&ndash;' + depths[i + 1] + '' : '+') + '</span></li>');
+  };
+
+    div.innerHTML += "<ul>" + labels.join("") + "</ul>";
+    return div;
+  };
+
+  // Adding the legend to the map
+  legend.addTo(myMap);
+}
+
